cf1c4d39ca594ad52aab67f527b24ea9e2773242
Zane Brown <zib1991@gmail.com>
2021-05-29 08:50:25 -0400

    Add header and background image

--- assets/js/components/Root.tsx
+++ assets/js/components/Root.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./Header"
+import background from "../../static/images/background.png";
 
 const Root = () => {
   return (
-    <div>
-      <p>Test</p>
+    <div className="w-screen h-screen p-52 bg-cover bg-fixed flex flex-col space-y-8" style={{backgroundImage: `url(${background})`}}>
+      <Header/>
     </div>
   );
 }
