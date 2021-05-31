a771275139a88c7342d70c4c8d5340203869534f
Zane Brown <zib1991@gmail.com>
2021-05-31 07:03:57 -0400

    Add more front end tests

--- test/byte_slyce_web/wallaby/url_form_test.exs
+++ test/byte_slyce_web/wallaby/url_form_test.exs
@@ -6,11 +6,29 @@ defmodule ByteSlyceWeb.UrlFormTest do
   @error Query.css(".error")
   @input Query.css("input")
 
-  feature "exists", %{session: session} do
+  feature "error dialog appears when invalid input is entered", %{session: session} do
     session
     |> visit("/")
     |> assert_has(@button)
     |> fill_in(@input, with: "aaaa")
+    |> assert_has(@error)
+    |> take_screenshot()
+  end
+
+  feature "error message does not appear when first few characters match http(s)://", %{session: session} do
+    session
+    |> visit("/")
+    |> fill_in(@input, with: "htt")
+    |> refute_has(@error)
+    |> take_screenshot()
+  end
+
+  feature "error message appears when server throws an error", %{session: session} do
+    session
+    |> visit("/")
+    |> assert_has(@button)
+    |> fill_in(@input, with: "htt")
+    |> click(@button)
     |> assert_has(@error)
     |> take_screenshot()
   end
